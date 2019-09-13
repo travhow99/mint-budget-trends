e06a62e8a50971d4056a634691602f2eed068b9c
travis <trav.horn@gmail.com>
2019-09-13 19:17:12 +0200

    relative directory paths

--- backend/import.php
+++ backend/import.php
@@ -4,15 +4,6 @@
 
     $file = $_POST['file'];
 
-    if (!file_exists('../uploads')) {
-        mkdir('../uploads', 0777, true);
-    }
-    if (!file_exists('../uploads'.'/'.$year)) {
-        echo 'making';
-        mkdir('../uploads'.'/'.$year, 0777, true);
-    }
-
-
     $target_dir = '../uploads/';
     $target_file = $target_dir . $year . '/' . $month . '.csv';
     $uploadOk = 1;
