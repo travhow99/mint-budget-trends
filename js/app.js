1b49f542b0f9ac296ebeac8fb2d5ae819859bd4d
travis <trav.horn@gmail.com>
2019-09-20 16:50:22 +0200

    Switch to global state

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+let state = {};
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 $('#home').click(function () {
     $('.dashboard .col-sm-10').hide();
@@ -17,8 +19,8 @@ $('#category').click(function () {
 });
 
 $('#upload').click(() => {
-	$('.dashboard .col-sm-10').hide();
-	$('.upload').show();
+    $('.dashboard .col-sm-10').hide();
+    $('.upload').show();
 });
 
 for (let x of monthNames) {
@@ -30,7 +32,7 @@ const date = new Date().getFullYear();
 for (let x = date; x > date - 5; x--) {
     // years.push(x);
     $('#yearSelect').append(`<option value='${x}'>${x}</option>`);
-} 
+}
 
 
 // Function needs to be promise to build dropdown after gathering filenames
@@ -40,13 +42,13 @@ async function orderMonths(obj) {
             const monthFiles = [];
             const foundMonths = Object.values(obj[year]);
 
-    /*         for (let month in obj[year]) {
-                const yr = obj[year];
-                console.log(yr[month]);
-                if (monthNames.indexOf(month.toLowerCase() >= 0)) {
-                    monthFiles.push(yr[month]);
-                }
-            } */
+            /*         for (let month in obj[year]) {
+                        const yr = obj[year];
+                        console.log(yr[month]);
+                        if (monthNames.indexOf(month.toLowerCase() >= 0)) {
+                            monthFiles.push(yr[month]);
+                        }
+                    } */
             for (let month in monthNames) {
                 const m = monthNames[month];
                 if (foundMonths.indexOf(m.toLowerCase()) >= 0) {
@@ -61,35 +63,35 @@ async function orderMonths(obj) {
 
 // Make full promise chain?
 orderMonths(uploads)
-                    .then((res) => buildMonthDropdown(res))
-                    .then((res) => gatherData(res))
-                    .then((obj) => gatherCategories(obj))
-                    .then(console.log)
-                    .catch(e => console.log('Error: ' + e));
+    .then((res) => {
+        buildMonthDropdown(res);
+        return gatherData(res);
+    })
+    // .then((res) => gatherData(res))
+    // .then((data) => gatherCategories(data))
+    .then(console.log)
+    .catch(e => console.log('Error: ' + e));
 
-/* async */ function buildMonthDropdown(obj) {
-    // return new Promise((resolve, reject) => {
-        const yearsAdded = [];
-        for (let year of Object.keys(obj)) {
-            const months = obj[year];
+function buildMonthDropdown(obj) {
+    const yearsAdded = [];
+    for (let year of Object.keys(obj)) {
+        const months = obj[year];
 
-            let tmpDropdown = `<option disabled value="${year}">${year}</option>`;
+        let tmpDropdown = `<option disabled value="${year}">${year}</option>`;
 
-            for (let month of months){
-                // $('.monthDropdown').append(`<option value="${month}">${month}</option>`);
-                tmpDropdown += `<option value="${month}_${year}">${month}</option>`;
-            }
-
-            if (year < yearsAdded[yearsAdded.length - 1]) {
-                $('.monthDropdown').append(tmpDropdown);
-            } else {
-                $('.monthDropdown').prepend(tmpDropdown);
-            }
-            yearsAdded.push(year);
+        for (let month of months) {
+            // $('.monthDropdown').append(`<option value="${month}">${month}</option>`);
+            tmpDropdown += `<option value="${month}_${year}">${month}</option>`;
         }
-        return obj;
-        // resolve(obj);
-    // });
+
+        if (year < yearsAdded[yearsAdded.length - 1]) {
+            $('.monthDropdown').append(tmpDropdown);
+        } else {
+            $('.monthDropdown').prepend(tmpDropdown);
+        }
+        yearsAdded.push(year);
+    }
+    // return obj;
 }
 
 // Take object
@@ -99,65 +101,108 @@ orderMonths(uploads)
 async function gatherData(obj) {
     const spendingData = {};
     return new Promise((resolve, reject) => {
-  
-      for (let year of Object.keys(obj)) {
-          const months = obj[year];
-          spendingData[year] = {};
 
-        for (let month of months) {
-            let m = month.toLowerCase();
+        for (let year of Object.keys(obj)) {
+            const months = obj[year];
+            spendingData[year] = {};
 
-            spendingData[year][m] = {};
-            let filePath = `./uploads/${year}/${m}.csv`;
-            Papa.parse(filePath, {
-                header: true, 
-                download: true,
-                skipEmptyLines: true,
-                complete: function(results) {
-                    results.data.map(a => {
-                        let {Category, Spending} = a;
-                        let cat = stringifyCategory(Category);
-                        spendingData[year][m][cat] = dollarToNum(Spending);
-                    });
-                    // console.log(res);
-                    
-                    // spendingData[year][month] = results.data;
-                }
-            });
+            // Promise.all here
+            (async function() {
+                // map through month
+                months.map(async month => {
+                    // build filepath
+                    const m = month.toLowerCase();
+
+                    spendingData[year][m] = {};
+                    const filePath = `./uploads/${year}/${m}.csv`;
+
+                    await Papa.parse(filePath, {
+                        header: true,
+                        download: true,
+                        skipEmptyLines: true,
+                        complete(results) {
+                            results.data.map(a => {
+                                let { Category, Spending } = a;
+                                let cat = stringifyCategory(Category);
+                                spendingData[year][m][cat] = dollarToNum(Spending);
+                            });
+                        }
+                    })
+    
+                });
+            })();
+
+/*             for (let month of months) {
+                let m = month.toLowerCase();
+
+                spendingData[year][m] = {};
+                let filePath = `./uploads/${year}/${m}.csv`;
+                Papa.parse(filePath, {
+                    header: true,
+                    download: true,
+                    skipEmptyLines: true,
+                    complete(results) {
+                        results.data.map(a => {
+                            let { Category, Spending } = a;
+                            let cat = stringifyCategory(Category);
+                            spendingData[year][m][cat] = dollarToNum(Spending);
+                        });
+                        // console.log(res);
+
+                        // spendingData[year][month] = results.data;
+                    }
+                });
+            }
+ */
         }
-
-      }
-      resolve(spendingData);
+        console.log(spendingData);
+        state = {spendingData, ...state};
+        console.log(state);
+        resolve(spendingData);
+        reject(error);
     });
 }
 
 // Function is logging before data is ready
 // Needs to be async
-async function gatherCategories(obj) {
+function gatherCategories(obj) {
+    console.log(obj);
     const categories = [];
     // let data = await Object.entries(obj);
     const breakdown = Object.values(obj);
-    await breakdown.forEach((year) => {
-        console.log(Object.values(year));
-        const months = Object.values(year);
-        
-        months.forEach((m) => {
-            const cats = Object.keys(m);
-            console.log(m);
+    try {
+        console.log('trying ', breakdown)
+        breakdown.forEach((year) => {
+            // console.log(Object.values(year));
+            const months = Object.values(year);
 
-            cats.forEach((cat) => {
-                if (categories.indexOf(cat) >= 0) {
-                    console.log('dup')
-                    return;
-                }
-                categories.push(cat);
+            months.forEach((m) => {
+                const cats = Object.keys(m);
+                console.log(m);
+                console.log(cats);
+
+                cats.forEach((cat) => {
+                    /* if (categories.indexOf(cat) >= 0) {
+                        // console.log('dup')
+                        return;
+                    } */
+                    console.log(cat);
+                    categories.push(cat);
+                })
             })
         })
-    })
-    console.log(categories);
+        if (categories.length > 0) {
+            return categories;
+        } else {
+            console.log('cate mpty')
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    // console.log(categories);
 
 
-    
+    return obj;
     return categories;
 
     return new Promise((resolve, reject) => {
@@ -180,9 +225,9 @@ async function gatherCategories(obj) {
                 console.log(Object.keys(m[1]));
                 for (let x of m) {
                     console.log(x)
-                    if (typeof(x) === 'object') {
+                    if (typeof (x) === 'object') {
                         console.log(x)
-                        let cats = {...x}
+                        let cats = { ...x }
                         console.log(cats);
                         console.log(Object.getOwnPropertyNames(x))
                     }
@@ -190,7 +235,7 @@ async function gatherCategories(obj) {
             }
 
             return;
-            
+
             for (let m of Object.keys(months)) {
                 console.log(m)
                 let val = months[m];
